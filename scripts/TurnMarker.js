0cc57c26084cb81a46708607ca91444506a0e7da
theripper93 <tsplab@gmail.com>
2021-07-21 01:37:12 +0200

    Add files via upload

---
+++ scripts/TurnMarker.js
@@ -0,0 +1,85 @@
+class TurnMarker {
+  constructor() {
+    this.token;
+    this.container = new PIXI.Container();
+    this.container.name = "CBTurnMarker";
+    this.container.zIndex = game.settings.get("combatbooster", "markerAbove") ? 1000:-1;
+    this.img = game.settings.get("combatbooster", "markerPath");
+    this.speed = game.settings.get("combatbooster", "markerSpeed") / 10;
+    this.scale = game.settings.get("combatbooster", "markerScale");
+    this.alpha = game.settings.get("combatbooster", "markerAlpha");
+    let video = this.img.endsWith("webm")
+      ? document.createElement("video")
+      : null;
+    if (video) {
+      video.src = this.img;
+      video.loop = true;
+    }
+    this.sprite = new PIXI.Sprite.from(video ?? this.img);
+    this.sprite.alpha = this.alpha;
+    this.sprite.width=400
+    this.sprite.height=400
+    this.baseScale = this.sprite.scale.x
+    this.sprite.anchor.set(0.5, 0.5);
+    this.container.addChild(this.sprite);
+    Object.defineProperty(this.container, "visible", {
+      get() {
+        return game.combat?.started;
+      },
+    });
+    let _this = this;
+    function Animate() {
+      if (_this.sprite._destroyed) {
+        canvas.app.ticker.remove(Animate);
+        if (!_this.sprite.reallyDestroy) new TurnMarker();
+      } else {
+        if (_this.container.visible)
+          _this.sprite.rotation += 0.01 * _this.speed;
+      }
+    }
+    canvas.tokens.CBTurnMarker = this;
+    canvas.app.ticker.add(Animate);
+    this.MoveToCombatant();
+  }
+  Move(token) {
+    this.token = token;
+    if (!token) return;
+    token.addChild(this.container);
+    this.Update();
+  }
+
+  Destroy(reallyDestroy) {
+    if (this.token) {
+      let child = this.token.children.find((c) => c.name === "CBTurnMarker");
+      this.token.removeChild(child);
+    }
+    this.sprite.reallyDestroy = reallyDestroy;
+    this.sprite.destroy();
+    this.container.destroy();
+    canvas.tokens.CBTurnMarker = null;
+  }
+
+  Update() {
+    if (this.container._destroyed) return;
+    this.container.x = this.token.w / 2;
+    this.container.y = this.token.h / 2;
+    this.token.sortableChildren = true;
+    this.sprite.width = canvas.dimensions.size * 1.4 * this.scale * this.tokenScale
+    this.sprite.height = canvas.dimensions.size * 1.4 * this.scale * this.tokenScale
+  }
+
+  MoveToCombatant() {
+    const combatant = canvas.tokens.get(game.combat?.combatant?._token?._id);
+    if (combatant && this.id !== combatant.id) this.Move(combatant);
+  }
+
+  get tokenScale() {
+    return (
+      Math.max(this.token.data.width, this.token.data.height) *
+      this.token.data.scale
+    );
+  }
+  get tokenId() {
+    return this.token?.id;
+  }
+}
---
+++ scripts/main.js
@@ -0,0 +1,36 @@
+const COMBAT_BOOSTER_MODULE_NAME = "combatbooster";
+
+/***************
+ * TURN MARKER *
+ ***************/
+
+//icons permission jinker, brimcon Wassily hobolyra cefasheli 
+//no permissions Rin 
+
+Hooks.on("canvasReady", function () {
+  if (game.settings.get("combatbooster", "enableMarker")) {
+    new TurnMarker();
+    Hooks.once("renderCombatTracker", function () {
+      if(canvas.tokens.CBTurnMarker && !canvas.tokens.CBTurnMarker.token)
+      canvas.tokens.CBTurnMarker.MoveToCombatant()
+    })
+  }
+});
+
+Hooks.on("updateCombat", function () {
+  if (game.settings.get("combatbooster", "enableMarker")) {
+    if (!canvas.tokens.CBTurnMarker) {
+      new TurnMarker();
+    } else {
+      canvas.tokens.CBTurnMarker.MoveToCombatant();
+    }
+  }
+});
+
+Hooks.on("updateToken", function (token, updates) {
+  if (token.id === canvas.tokens.CBTurnMarker?.token?.id) {
+    if ("scale" in updates) {
+      canvas.tokens.CBTurnMarker.Update();
+    }
+  }
+});
