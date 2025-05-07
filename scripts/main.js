94049329ae91698721a2d0bd2cde891ad474ecb8
theripper93 <tsplab@gmail.com>
2025-05-07 17:44:42 +0200

    a

--- scripts/main.js
+++ scripts/main.js
@@ -4,15 +4,23 @@ const COMBAT_BOOSTER_MODULE_NAME = "combatbooster";
  * TURN MARKER *
  ***************/
 
-//icons permission jinker, brimcon Wassily hobolyra cefasheli 
-//no permissions Rin 
+//icons permission jinker, brimcon Wassily hobolyra cefasheli
+//no permissions Rin
 
-function regenerateMarkers(){
-  if(!game.combat?.started) return;
-  if(!game.settings.get("combatbooster", "enableMarker")) return;
-  if(!canvas.tokens.CBTurnMarker) new TurnMarker();
-  if(!canvas.tokens.CBStartTurnMarker && game.settings.get("combatbooster", "enableStartMarker")) new StartTurnMarker();
-  if(!canvas.tokens.CBNextTurnMarker && game.settings.get("combatbooster", "enableNextMarker")) new NextTurnMarker();
+function regenerateMarkers() {
+  if (!game.combat?.started) return;
+  if (!game.settings.get("combatbooster", "enableMarker")) return;
+  if (!canvas.tokens.CBTurnMarker) new TurnMarker();
+  if (
+    !canvas.tokens.CBStartTurnMarker &&
+    game.settings.get("combatbooster", "enableStartMarker")
+  )
+    new StartTurnMarker();
+  if (
+    !canvas.tokens.CBNextTurnMarker &&
+    game.settings.get("combatbooster", "enableNextMarker")
+  )
+    new NextTurnMarker();
   canvas.tokens.CBTurnMarker?.MoveToCombatant();
   canvas.tokens.CBStartTurnMarker?.MoveToCombatant();
   canvas.tokens.CBNextTurnMarker?.MoveToCombatant();
@@ -21,7 +29,7 @@ function regenerateMarkers(){
 Hooks.on("canvasReady", function () {
   if (game.settings.get("combatbooster", "enableMarker")) {
     new TurnMarker();
-    
+
     if (game.settings.get("combatbooster", "enableStartMarker")) {
       new StartTurnMarker();
     }
@@ -30,10 +38,19 @@ Hooks.on("canvasReady", function () {
     }
     Hooks.once("renderCombatTracker", function () {
       regenerateMarkers();
-      if(canvas.tokens.CBTurnMarker && !canvas.tokens.CBTurnMarker.token)
-      canvas.tokens.CBTurnMarker.MoveToCombatant()
-      canvas.tokens.CBNextTurnMarker?.MoveToCombatant()
+      if (canvas.tokens.CBTurnMarker && !canvas.tokens.CBTurnMarker.token)
+        canvas.tokens.CBTurnMarker.MoveToCombatant();
+      canvas.tokens.CBNextTurnMarker?.MoveToCombatant();
     });
+  }
+});
+
+Hooks.once("ready", () => {
+  if (game.user.isGM && game.settings.get("combatbooster", "enableMarker") && game.settings.get("core", "combatTrackerConfig")?.turnMarker?.enabled) {
+    const sett = game.settings.get("core", "combatTrackerConfig");
+    sett.turnMarker.enabled = false;
+    game.settings.set("core", "combatTrackerConfig", sett);
+    ui.notifications.warn(game.i18n.localize("combatbooster.coreMarker"), { permanent: true });
   }
 });
 
@@ -45,8 +62,8 @@ Hooks.on("updateToken", function (token, updates) {
   if (token.id === canvas.tokens.CBTurnMarker?.token?.id) {
     if ("texture" in updates || "width" in updates || "height" in updates) {
       canvas.tokens.CBTurnMarker.Update();
-      canvas.tokens.CBNextTurnMarker?.Update()
-      canvas.tokens.CBStartTurnMarker?.Update()
+      canvas.tokens.CBNextTurnMarker?.Update();
+      canvas.tokens.CBStartTurnMarker?.Update();
     }
   }
 });
@@ -57,19 +74,19 @@ Hooks.on("deleteToken", (token) => {
 
 Hooks.on("combatTurn", () => {
   Hooks.once("updateCombat", () => {
-    canvas.tokens.CBStartTurnMarker?.MoveToCombatant()
-    canvas.tokens.CBNextTurnMarker?.MoveToCombatant()
+    canvas.tokens.CBStartTurnMarker?.MoveToCombatant();
+    canvas.tokens.CBNextTurnMarker?.MoveToCombatant();
   });
-})
+});
 Hooks.on("combatRound", () => {
   Hooks.once("updateCombat", () => {
-    canvas.tokens.CBStartTurnMarker?.MoveToCombatant()
-    canvas.tokens.CBNextTurnMarker?.MoveToCombatant()
+    canvas.tokens.CBStartTurnMarker?.MoveToCombatant();
+    canvas.tokens.CBNextTurnMarker?.MoveToCombatant();
   });
-})
- 
+});
+
 Hooks.on("getSwadeCombatTrackerEntryContext", function () {
-  try {    
+  try {
     if (game.settings.get("combatbooster", "enableMarker")) {
       if (!canvas.tokens.CBTurnMarker) {
         new TurnMarker();
@@ -81,9 +98,8 @@ Hooks.on("getSwadeCombatTrackerEntryContext", function () {
         }
       } else {
         canvas.tokens.CBTurnMarker.MoveToCombatant();
-        canvas.tokens.CBNextTurnMarker?.MoveToCombatant()
+        canvas.tokens.CBNextTurnMarker?.MoveToCombatant();
       }
     }
-  }catch (error) {
-  }
+  } catch (error) {}
 });
